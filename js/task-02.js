555e39347e4f7d3acc5c175ae78b6c4702d394ba
Андрій <rukom@ukr.net>
2023-10-06 18:16:27 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -8,3 +8,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector('#ingredients');
+
+const arrIngredients = ingredients.map(ingredient => createItem(ingredient));
+
+list.append(...arrIngredients);
+
+function createItem(elem) {
+  const listItem = document.createElement('li');
+  listItem.textContent = elem;
+  listItem.classList.add('item');
+  return listItem;
+}

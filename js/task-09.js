8f5cb99d0d7f15abf3a7d57631553873c2fa6762
Андрій <rukom@ukr.net>
2023-10-08 22:46:58 +0300

    Executed task-09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const body = document.querySelector('body');
+const btnChangeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color');
+
+btnChangeColor.addEventListener('click', () => {
+  body.style.backgroundColor = getRandomHexColor();
+  spanColor.textContent = getRandomHexColor();
+})

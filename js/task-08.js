f3ef5728459f3cc14f272cdd349a11f4c7e8dcf3
Андрій <rukom@ukr.net>
2023-10-08 22:35:59 +0300

    Executed task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+'use strict';
 
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  const formObject = {};
+
+  if (!email || !password) {
+    alert('Please fill in all the fields!');
+  } else {
+    formObject[form.elements.email.name] = email;
+    formObject[form.elements.password.name] = password;
+    console.log(formObject);
+  }
+
+  loginForm.reset();
+})

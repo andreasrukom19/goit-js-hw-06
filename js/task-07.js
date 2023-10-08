61610854325bb5f253e4f2ca3c283c88d9995f02
Андрій <rukom@ukr.net>
2023-10-08 22:04:32 +0300

    Update task-07.js

---
+++ js/task-07.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const slider = document.querySelector('#font-size-control');
+const text = document.querySelector('#text');
+
+slider.addEventListener('input', (event) => {
+  text.style.fontSize = `${event.currentTarget.value}px`;
+})

50dd85e1cdde7ed0b135625178314383a9d0ccdd
Андрій <rukom@ukr.net>
2023-10-09 01:31:06 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,36 @@
+'use strict';
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const controlsDiv = document.querySelector('#controls');
+const fields = controlsDiv.querySelector('input');
+const createBtn = controlsDiv.querySelector('button[data-create]');
+const destroyBtn = controlsDiv.querySelector('button[data-destroy]');
+const divBox = document.querySelector('#boxes');
+let number;
+
+createBtn.addEventListener('click', (event) => {
+  const number = event.target.previousElementSibling.value;
+  createBoxes(number);
+});
+
+function createBoxes(amount) {
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    divBox.append(box);
+    size += 10;
+  }
+}
+
+destroyBtn.addEventListener('click', () => {
+  divBox.innerHTML = '';
+})

44983879db6aa860127e2ecbd0a6add7de9c3e10
Андрій <rukom@ukr.net>
2023-10-10 13:35:07 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -5,18 +5,18 @@ const loginForm = document.querySelector('.login-form');
 loginForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const form = event.target;
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
-
-  const formObject = {};
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
 
   if (!email || !password) {
-    alert('Please fill in all the fields!');
-  } else {
-    formObject[form.elements.email.name] = email;
-    formObject[form.elements.password.name] = password;
-    console.log(formObject);
+    return alert('Please fill in all the fields!');
   }
 
-  loginForm.reset();
+  const formObject = {
+    [form.elements.email.name]: email,
+    [form.elements.password.name]: password,
+  };
+  console.log(formObject);
+
+  event.target.reset();
 })

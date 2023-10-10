76b5f72043a9e8fee913b375b9af6d8b4459091e
Андрій <rukom@ukr.net>
2023-10-10 13:34:59 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,17 +1,18 @@
 'use strict'
 
 const inputField = document.querySelector('#validation-input');
-const strLength = inputField.dataset.length;
+const dataLength = Number(inputField.dataset.length);
 
 inputField.addEventListener('blur', (event) => {
-  if (event.currentTarget.value.length != strLength && event.currentTarget.value.length != 0) {
-    inputField.classList.add('invalid');
-    inputField.classList.remove('valid');
-  } else if (event.currentTarget.value.length == strLength) {
-    inputField.classList.add('valid');
-    inputField.classList.remove('invalid');
+  const resultLength = event.currentTarget.value.length;
+  if (resultLength === dataLength) {
+    changeClass('valid', 'invalid');
   } else {
-    inputField.classList.remove('valid');
-    inputField.classList.remove('invalid');
+    changeClass('invalid', 'valid');
   }
 })
+
+function changeClass(cl1, cl2) {
+  inputField.classList.add(cl1);
+  inputField.classList.remove(cl2);
+}

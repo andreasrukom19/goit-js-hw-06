d7043a431308906f88fdfd121b3a30015ff07e3f
Андрій <rukom@ukr.net>
2023-10-10 13:34:06 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -4,7 +4,7 @@ const nameInput = document.querySelector('#name-input');
 const nameOutput = document.querySelector('#name-output');
 
 nameInput.addEventListener('input', (event) => {
-  if (!event.currentTarget.value) {
+  if (!event.currentTarget.value.trim()) {
     nameOutput.textContent = 'Anonymous';
   } else {
     nameOutput.textContent = event.currentTarget.value;
